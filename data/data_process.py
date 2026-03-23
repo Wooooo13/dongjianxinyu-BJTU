d75bb35165519aa5461e9f981dd1da55999e386c
Wooooo13 <111061168+Wooooo13@users.noreply.github.com>
2026-03-23 14:04:20 +0800

    Add files via upload

---
+++ data/data_process.py
@@ -0,0 +1,28 @@
+import random
+#
+data = []
+# with open("neutral.csv","r",encoding="utf-8") as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         line = line.strip()
+#         line = line.strip().replace("\t", " ")
+#         data.append(line)
+# with open("all.csv","a",encoding="utf-8") as f:
+#     for line in data:
+#
+#         f.write(line+"\t1")
+#         f.write("\n")
+
+
+
+#
+# with open("all.csv","r",encoding="utf-8") as f:
+#     lines = f.readlines()
+#     for line in lines:
+#         line = line.strip()
+#         data.append(line)
+# random.shuffle(data)
+# with open("data.csv","a",encoding="utf-8") as f:
+#     for line in data:
+#         f.write(line)
+#         f.write("\n")
